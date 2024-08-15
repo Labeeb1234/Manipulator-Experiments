f96528745d147cfe482e9f896be9ac1e44e1544e
Labeeb Nassar <132001019@smail.iitpkd.ac.in>
2024-08-15 16:09:39 +0530

    Add files via upload

--- cust_test.py
+++ cust_test.py
@@ -15,10 +15,10 @@ from uarm.utils.log import logger
 swift = SwiftAPI(filters={'hwid': 'USB VID:PID=2341:0042'}, callback_thread_pool_size=0, do_not_open=True)
 
 def set_initial_endPose(swift, x, y, z):
-    result = swift.set_position(x=x, y=y, z=z, wait=True, timeout=10)
+    swift.set_position(x=x, y=y, z=z)
+    result = swift.flush_cmd(timeout=10, wait_stop=True)
     if result == 'OK':
-        current_arm_pose = swift.get_position()
-        print(f"x: {current_arm_pose[0]}, y: {current_arm_pose[1]}, z: {current_arm_pose[2]}")
+        print(f"{swift.get_position()}")
         print("All commands completed successfully and robot is stopped.")
         return True
     elif result == 'TIMEOUT':
@@ -90,8 +90,8 @@ def main():
             swift.connect(port='/dev/ttyACM0', baudrate=115200)
             # Wait until the robot is ready
             while(swift.waiting_ready() == False):
-                print(f"waiting for arm to be ready.....")
-            
+                print(f"waiting for arm to be ready.....")  
+                        
             swift.set_speed_factor(1)
             swift.set_mode(0)
             # system status
@@ -100,14 +100,13 @@ def main():
             servo_info = swift.get_servo_attach()
             mode = swift.get_mode()
             gripper_status = swift.get_gripper_catch()
-            
+            current_pose = swift.get_position()
             if(mode == 0):
-                pose_flag = swift.check_pos_is_limit([100.0, 50.0, 90.0])
-                print(f"{pose_flag}")
-                #func = set_initial_endPose(swift=swift, x=100.0, y=50.0, z=90.0)
-                # if func:
-                #     break
-            
+                print(f"{current_pose}")
+                func = set_initial_endPose(swift=swift, x=100.0, y=50.0, z=90.0)
+                if func:
+                    break
+                
 
         except Exception as e:
             # Handle errors (e.g., connection issues)
@@ -116,8 +115,6 @@ def main():
         finally:
             # Always disconnect after each loop iteration to avoid leaving the connection open
             swift.reset()
-            time.sleep(5.0)
-            swift.flush_cmd(timeout=10, wait_stop=True)
             swift.disconnect()
         
         # Wait until the next loop iteration
